e693fe88b8d7d0ff06f1e1d7b10fb22323adc36d
asreek <anjana_sreekumar@infosys.com>
2020-01-22 18:01:23 -0700

    review comment fix for monitoring

--- include/common/monitor_message.h
+++ include/common/monitor_message.h
@@ -1,19 +1,21 @@
 /*
- * Copyright (c) 2003-2018, Great Software Laboratory Pvt. Ltd.
- * Copyright (c) 2017 Intel Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2019-present Open Networking Foundation
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
 #ifndef __MONITOR_MSG_H_
 #define __MONITOR_MSG_H_
@@ -22,20 +24,21 @@
 #include "s1ap_structs.h"
 #include "s11_structs.h"
 
-/*Information needed for AIR*/
+/*Monitoring msg header*/
 enum e_BUF_HDR {
 	MONITOR_IMSI_REQ,
 	MONITOR_IMSI_RSP,
 };
 
 #define IMSI_STR_LEN 16
-/*AIR  - request. Struct is same for ULR*/
+
+/*Monitor IMSI Request*/
 struct monitor_imsi_req {
     int  req_type;
 	char imsi[IMSI_STR_LEN];
 };
 
-/*AIA  - response*/
+/*Monitor IMSI Response*/
 struct monitor_imsi_rsp {
         int             result;
 	unsigned char   imsi[IMSI_STR_LEN];
@@ -49,6 +52,7 @@ struct monitor_imsi_rsp {
 	struct fteid    s1u_enb_u_fteid;*/
 };
 
+/*Monitor IMSI List Response*/
 struct monitor_imsi_list_rsp {
     int             no_of_ue;
 	unsigned char   **imsi_list;
@@ -69,7 +73,6 @@ struct monitor_resp_msg {
 	}data;
 };
 
-#define MONITOR_RCV_BUF_SIZE  128
 #define MONITOR_REQ_MSG_SIZE  sizeof(struct monitor_req_msg)
 #define MONITOR_RESP_MSG_SIZE sizeof(struct monitor_resp_msg)
 
