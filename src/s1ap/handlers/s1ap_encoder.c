739cc803846d67efb282173018257d45f17536d7
Pingping Lin <pingping@opennetworking.org>
2020-04-21 19:20:32 -0700

    fix the UEIdentityIndexValue in paging
      the caculation of the UEIdentityIndexValue was wrong.

--- src/s1ap/handlers/s1ap_encoder.c
+++ src/s1ap/handlers/s1ap_encoder.c
@@ -1,18 +1,9 @@
 /*
+ * Copyright 2019-present Open Networking Foundation
  * Copyright (c) 2003-2018, Great Software Laboratory Pvt. Ltd.
  * Copyright (c) 2017 Intel Corporation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 
@@ -589,7 +580,7 @@ int s1ap_mme_encode_paging_request(
     /* index(10bit) = ue_imsi_value mod 1024 */
     uint16_t index_value = ue_imsi_value % 1024;
     UEIdentityIndexValue->buf[0] = index_value >> 2;
-    UEIdentityIndexValue->buf[1] = (index_value & 0x3f) << 6;
+    UEIdentityIndexValue->buf[1] = (index_value & 0x3) << 6;
     UEIdentityIndexValue->bits_unused = 6;
 
     log_msg(LOG_DEBUG,"Encoding STMSI\n");
